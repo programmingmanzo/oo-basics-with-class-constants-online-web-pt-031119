71e6480d0daaf52b47aa4cb2edb677b467c40264
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-28 11:13:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,7 +8,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if !(BRANDS.include?(@brand))
+    if !(BRANDS.include?(@brand)) #Checks to see if the brand is included in the constant class array already. If it is not then it gets shoveled into the constant class. 
     BRANDS << @brand
     end 
   end
