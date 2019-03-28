3e415d4b0390f618f7d7c21a39e09fbe127e4188
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-28 11:13:46 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,9 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre #Added genre here on the reader as we still need it 
+  
+  GENRES = [] #creates a class constant - written in all CAPS. 
+
 
   def initialize(title)
     @title = title
@@ -9,5 +12,10 @@ class Book
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
+  
+  def genre=(genre) #Created a writer since it required logic 
+    @genre = genre 
+    GENRES << genre #Shovels the different genre's into the empty class constant array 
+  end 
 
 end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,16 @@
+require 'pry'
+
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = [] 
 
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(@brand)) #Checks to see if the brand is included in the constant class array already. If it is not then it gets shoveled into the constant class. 
+    BRANDS << @brand
+    end 
   end
 
   def cobble
